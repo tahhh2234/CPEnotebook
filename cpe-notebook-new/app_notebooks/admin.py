98cd83b6d9bd22e0efa28a0f8ac93212b851b9b4
tahhh2234 <thadpongsermsook1@gmail.com>
2024-01-29 13:53:44 +0700

    add image to each notebook

--- cpe-notebook-new/app_notebooks/admin.py
+++ cpe-notebook-new/app_notebooks/admin.py
@@ -4,8 +4,8 @@ from app_notebooks.models import Notebook
 # Register your models here.
 
 class NotebookAdmin(admin.ModelAdmin):
-    list_display = ['title', 'chapters', 'is_maths']
-    search_fields = ['title']
+    list_display = ['title', 'code', 'chapters', 'is_maths']
+    search_fields = ['title' , 'code']
     list_filter = ['is_maths']
 
 admin.site.register(Notebook, NotebookAdmin)
--- cpe-notebook-new/app_notebooks/models.py
+++ cpe-notebook-new/app_notebooks/models.py
@@ -2,9 +2,11 @@ from django.db import models
 
 class Notebook(models.Model):
     title = models.CharField(max_length=255)
+    code = models.CharField(max_length=20, null=True, blank=True)
     chapters = models.IntegerField()
     is_maths = models.BooleanField(default=False)
     description = models.TextField(null=True, blank=True)
+    image_relative_url = models.CharField(max_length=100, null=True, blank=True)
 
     def __str__(self) -> str:
-        return '{} (id={})'.format(self.title, self.id)
+        return '{} {}'.format( self.code, self.title)
